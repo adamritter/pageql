7853c268750d1d7fa694cd284d3c12352eea63e9
adamritter <58403584+adamritter@users.noreply.github.com>
2025-05-16 16:04:26 +0200

    Separate build_ast

--- src/pageql/__init__.py
+++ src/pageql/__init__.py
@@ -5,7 +5,6 @@ PageQL: A template language for embedding SQL inside HTML directly
 # Import the main classes from the PageQL modules
 from .pageql import PageQL, RenderResult
 from .pageqlapp import PageQLApp
-
 # Define the version
 __version__ = "0.1.0"
 

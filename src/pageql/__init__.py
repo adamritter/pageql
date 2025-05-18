613169f1f9b89cb7c8e044b56f9caec7d6330314
adamritter <58403584+adamritter@users.noreply.github.com>
2025-05-18 11:12:13 +0200

    Add reactive SQL parser

--- src/pageql/__init__.py
+++ src/pageql/__init__.py
@@ -5,8 +5,9 @@ PageQL: A template language for embedding SQL inside HTML directly
 # Import the main classes from the PageQL modules
 from .pageql import PageQL, RenderResult
 from .pageqlapp import PageQLApp
+from .reactive_sql import parse_reactive
 # Define the version
 __version__ = "0.1.0"
 
 # Make these classes available directly from the package
-__all__ = ["PageQL", "RenderResult", "PageQLApp"]
+__all__ = ["PageQL", "RenderResult", "PageQLApp", "parse_reactive"]

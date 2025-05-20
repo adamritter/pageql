1f74eb41ffd8204b26122c32e3b665bc9507f57d
adamritter <58403584+adamritter@users.noreply.github.com>
2025-05-20 13:13:19 +0200

    Add ReadOnly wrapper for row columns

--- src/pageql/__init__.py
+++ src/pageql/__init__.py
@@ -3,11 +3,11 @@ PageQL: A template language for embedding SQL inside HTML directly
 """
 
 # Import the main classes from the PageQL modules
-from .pageql import PageQL, RenderResult
+from .pageql import PageQL, RenderResult, ReadOnly
 from .pageqlapp import PageQLApp
 from .reactive_sql import parse_reactive
 # Define the version
 __version__ = "0.1.0"
 
 # Make these classes available directly from the package
-__all__ = ["PageQL", "RenderResult", "PageQLApp", "parse_reactive"]
+__all__ = ["PageQL", "RenderResult", "ReadOnly", "PageQLApp", "parse_reactive"]

223d45de01dc05cb15f9ef4025ac6106b2b824e6
adamritter <58403584+adamritter@users.noreply.github.com>
2025-06-01 02:00:21 -0500

    Remove tokens and sessions modules

--- src/pageql/__init__.py
+++ src/pageql/__init__.py
@@ -7,20 +7,6 @@ from .pageql import PageQL, RenderResult
 from .reactive import ReadOnly
 from .pageqlapp import PageQLApp
 from .reactive_sql import parse_reactive
-from .tokens import (
-    generate_token,
-    hash_token,
-    create_tokens_table,
-    store_token,
-    get_and_delete_token,
-)
-from .sessions import (
-    create_session,
-    validate_session,
-    extend_session,
-    delete_session,
-    invalidate_user_sessions,
-)
 # Define the version
 __version__ = "0.1.0"
 
@@ -31,14 +17,4 @@ __all__ = [
     "ReadOnly",
     "PageQLApp",
     "parse_reactive",
-    "generate_token",
-    "hash_token",
-    "create_tokens_table",
-    "store_token",
-    "get_and_delete_token",
-    "create_session",
-    "validate_session",
-    "extend_session",
-    "delete_session",
-    "invalidate_user_sessions",
 ]

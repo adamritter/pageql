4a4ca980d03c490088c966657179e34b0172eb92
adamritter <58403584+adamritter@users.noreply.github.com>
2025-06-07 01:01:21 -0500

    Add async wrappers for PageQL rendering

--- src/pageql/__init__.py
+++ src/pageql/__init__.py
@@ -4,6 +4,7 @@ PageQL: A template language for embedding SQL inside HTML directly
 
 # Import the main classes from the PageQL modules
 from .pageql import PageQL
+from .pageql_async import PageQLAsync
 from .render_context import RenderResult
 from .reactive import ReadOnly
 from .pageqlapp import PageQLApp
@@ -15,6 +16,7 @@ __version__ = "0.1.0"
 # Make these classes available directly from the package
 __all__ = [
     "PageQL",
+    "PageQLAsync",
     "RenderResult",
     "ReadOnly",
     "PageQLApp",

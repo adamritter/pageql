c1ef3599f0b9160d61cd6d5887526681cb8ce7cc
adamritter <58403584+adamritter@users.noreply.github.com>
2025-06-26 05:08:55 -0500

    Remove async PageQL

--- src/pageql/__init__.py
+++ src/pageql/__init__.py
@@ -4,7 +4,6 @@ PageQL: A template language for embedding SQL inside HTML directly
 
 # Import the main classes from the PageQL modules
 from .pageql import PageQL
-from .pageql_async import PageQLAsync
 from .render_context import RenderResult
 from .reactive import ReadOnly
 from .pageqlapp import PageQLApp
@@ -17,7 +16,6 @@ __version__ = "0.1.0"
 # Make these classes available directly from the package
 __all__ = [
     "PageQL",
-    "PageQLAsync",
     "RenderResult",
     "ReadOnly",
     "PageQLApp",

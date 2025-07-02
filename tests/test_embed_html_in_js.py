3aa5e7efc29cdab5fb1fa022b3d0762b4fddee63
adamritter <58403584+adamritter@users.noreply.github.com>
2025-07-02 10:08:48 -0500

    Escape closing script tags case-insensitively

--- tests/test_embed_html_in_js.py
+++ tests/test_embed_html_in_js.py
@@ -5,3 +5,7 @@ from pageql.render_context import embed_html_in_js, escape_script
 def test_embed_html_in_js_matches_escape_script_plus_json_dumps():
     s = "<div><script></script></div>"
     assert embed_html_in_js(s) == escape_script(json.dumps(s))
+
+
+def test_escape_script_ignores_case():
+    assert escape_script("</SCRIPT>") == "<\\/script>"

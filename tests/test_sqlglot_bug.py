4db61d0cadbd3f5f9f455df14692e7ec53968c14
adamritter <58403584+adamritter@users.noreply.github.com>
2025-07-21 08:36:32 +0200

    Mark sqlglot EXISTS test as xfail

--- tests/test_sqlglot_bug.py
+++ tests/test_sqlglot_bug.py
@@ -1,7 +1,9 @@
+import pytest
 import sqlglot
 from sqlglot import expressions as exp
 
 
+@pytest.mark.xfail(reason="sqlglot incorrectly handles EXISTS subquery counting")
 def test_findall_exists_subquery():
     sql = "select * from tweets where exists(select 1 from tweets t2 where t2.id = tweets.id)"
     expr = sqlglot.parse_one(sql, read="sqlite")
